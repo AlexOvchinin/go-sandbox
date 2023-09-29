3c85e0429035b74878c10395a851b9f541bbbc39
Aleksandr Ovchinin <alex.ovchinin@gmail.com>
2023-09-29 14:16:28 +0500

    Load chats on startup, save on every change

--- pingtelegrambot/go.mod
+++ pingtelegrambot/go.mod
@@ -2,7 +2,4 @@ module fm/pingtelegrambot
 
 go 1.20
 
-require (
-	github.com/deckarep/golang-set/v2 v2.3.1
-	gopkg.in/telebot.v3 v3.1.3
-)
+require gopkg.in/telebot.v3 v3.1.3
--- pingtelegrambot/main.go
+++ pingtelegrambot/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"time"
 
-	mapset "github.com/deckarep/golang-set/v2"
 	tele "gopkg.in/telebot.v3"
 )
 
 type Chat struct {
 	ID        int64
-	usernames mapset.Set[string]
-	users     map[int64]string
+	Usernames map[string]struct{}
+	Users     map[int64]string
 }
 
 var chats = make(map[int64]*Chat)
+var dataPath = os.Getenv("PING_BOT_DATA_PATH")
+var Empty struct{}
 
 func main() {
 	token := os.Getenv("PING_BOT_TOKEN")
@@ -32,71 +34,109 @@ func main() {
 		return
 	}
 
+	loadChats()
+
 	b.Handle("/add", handleAddCommand)
 	b.Handle("/everyone", handleEveryoneCommand)
 	b.Handle(tele.OnUserJoined, handleUserJoined)
 	b.Handle(tele.OnUserLeft, handleUserLeft)
 
 	b.Start()
+
+	log.Println("Ping Telegram Bot Started")
+}
+
+func saveChats() {
+	f, err := os.Create(dataPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	marshaledChats, err := json.Marshal(chats)
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = f.WriteString(string(marshaledChats))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func loadChats() {
+	marshaledChats, err := os.ReadFile(dataPath)
+	if err != nil {
+		log.Println(err)
+		fmt.Printf("No chats to load")
+		return
+	}
+
+	err = json.Unmarshal(marshaledChats, &chats)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Loaded %v chats", len(chats))
 }
 
 func handleAddCommand(ctx tele.Context) error {
-	chat := chats[ctx.Chat().ID]
-	if chat == nil {
-		chat = &Chat{ctx.Chat().ID, mapset.NewSet[string](), make(map[int64]string)}
-		chats[ctx.Chat().ID] = chat
-	}
+	chat := getChat(ctx.Chat().ID)
 	for _, entity := range ctx.Message().Entities {
 		switch entity.Type {
 		case tele.EntityMention:
-			chat.usernames.Add(ctx.Message().EntityText(entity))
+			chat.Usernames[ctx.Message().EntityText(entity)] = Empty
 		case tele.EntityTMention:
-			fmt.Println(entity.User)
-			chat.users[entity.User.ID] = entity.User.FirstName
-			chat.usernames.Remove(entity.User.Username)
+			chat.Users[entity.User.ID] = entity.User.FirstName
+			delete(chat.Usernames, entity.User.Username)
 		}
 	}
+	go saveChats()
 	return ctx.Send("Added")
 }
 
 func handleEveryoneCommand(ctx tele.Context) error {
-	chat := chats[ctx.Chat().ID]
-	if chat == nil {
-		return ctx.Send("Noone to mention. Please use /add to add users to mention manually")
-	}
+	chat := getChat(ctx.Chat().ID)
 	var builder strings.Builder
-	usernamesIter := chat.usernames.Iterator()
-	for elem := range usernamesIter.C {
+	for elem, _ := range chat.Usernames {
 		fmt.Fprintf(&builder, "%v", elem)
 		fmt.Fprintf(&builder, " ")
 	}
-	for userId, username := range chat.users {
+	for userId, username := range chat.Users {
 		fmt.Fprintf(&builder, "[%v](tg://user?id=%v)", username, userId)
 		fmt.Fprintf(&builder, " ")
 	}
+	message := builder.String()
+	if len(message) == 0 {
+		return ctx.Send("Noone to mention. Please use /add to add users to mention manually")
+	}
+
 	return ctx.Send(builder.String(), tele.ModeMarkdownV2)
 }
 
 func handleUserJoined(ctx tele.Context) error {
-	chat := chats[ctx.Chat().ID]
-	if chat == nil {
-		chat = &Chat{ctx.Chat().ID, mapset.NewSet[string](), make(map[int64]string)}
-		chats[ctx.Chat().ID] = chat
-	}
+	chat := getChat(ctx.Chat().ID)
 	joinedUser := ctx.Message().UserJoined
-	chat.users[joinedUser.ID] = joinedUser.FirstName
-	chat.usernames.Remove(joinedUser.Username)
+	chat.Users[joinedUser.ID] = joinedUser.FirstName
+	delete(chat.Usernames, joinedUser.Username)
+	go saveChats()
 	return nil
 }
 
 func handleUserLeft(ctx tele.Context) error {
-	chat := chats[ctx.Chat().ID]
-	if chat == nil {
-		chat = &Chat{ctx.Chat().ID, mapset.NewSet[string](), make(map[int64]string)}
-		chats[ctx.Chat().ID] = chat
-	}
+	chat := getChat(ctx.Chat().ID)
 	leftUser := ctx.Message().UserLeft
-	delete(chat.users, leftUser.ID)
-	chat.usernames.Remove(leftUser.Username)
+	delete(chat.Users, leftUser.ID)
+	go saveChats()
 	return nil
+}
+
+func getChat(id int64) *Chat {
+	chat := chats[id]
+	if chat == nil {
+		chat = &Chat{id, make(map[string]struct{}), make(map[int64]string)}
+		chats[id] = chat
+	}
+	return chat
 }
